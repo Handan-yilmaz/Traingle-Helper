54312567ed6ada790de34af13e51cebee922dcf5
Handan Yilmaz <Handanyilmazhy@gmail.com>
2025-10-08 14:45:31 +0300

    Add files via upload

---
+++ src/traingle/helper/TraingleHelper.java
@@ -0,0 +1,24 @@
+package traingle.helper;
+
+import java.util.Scanner;
+
+public class TraingleHelper 
+{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int a;
+        int b;
+        System.out.println(" First Side : ");
+        a = scanner.nextInt();
+        System.out.println(" Second Side : ");
+        b = scanner.nextInt();
+        
+        double h = Math.sqrt( a * a + b * b) ;
+        System.out.println("Hypotenuse : " + h );
+        
+        
+    }
+    
+}
